adc44a5f87c7ba7ec9ee137107741d06e51111a3
Jonas Roels <jonas.roels@student.hogent.be>
2023-03-01 19:58:06 +0100

    Created FirstClass close #1

---
+++ Howest.Prog.CoinChop.Core/FirstClass/FirstClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Howest.Prog.CoinChop.Core.FirstClass
+{
+    public class FirstClass
+    {
+        public FirstClass()
+        {
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("FirstClass");
+        }
+    }
+}
